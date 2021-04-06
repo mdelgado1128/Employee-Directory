6a53211c38fde4c7a2d51cbc0611995d08e0592b
Mark Delgado <markdelgado94@gmail.com>
2021-04-05 22:49:48 -0700

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 import React , {useEffect, useState} from "react";
 
 
 function App() {
   const [employees, setEmployees] = useState([]);
+  const [myfilter, setMyFilter] = useState("");
   useEffect(() => {
 const api = 'https://randomuser.me/api/?results=5'
 fetch(api).then((res) => res.json()).then((data)=> {
@@ -12,23 +12,38 @@ fetch(api).then((res) => res.json()).then((data)=> {
   setEmployees(data.results)
 })
   },[])
+  function changeFilter (event) {
+    setMyFilter(event.target.value)
+  }
   return (
     <div className="App">
-      <table>
+       <h1 className="jumbotron">Employee Directory</h1>
+     <div className="container heading row col jumbotron">
+       <p>Search by a employees name: </p>
+       <input value={myfilter} onChange={changeFilter}/>
+     </div>
+      <div className="table1 container">  
+      <table className="table table-hover jumbotron">
+        <thread>
         <tr>
-          <th>Image</th>
-          <th>Name</th>
-          <th>Age</th>
+          <th scope="col">Name</th>
+          <th scope="col">Age</th>
+          <th scope="col">Image</th>
         </tr>
+      </thread>
       
-     {employees.map(employee => (
+     {employees.filter((employee)=> (employee.name.first + " " + employee.name.last).toLowerCase().includes(myfilter.toLowerCase())).map(employee => (
+      <tbody> 
       <tr>
-        <tb><img src={employee.picture.medium} /></tb>
-      <tb>{employee.name.first} {employee.name.last}</tb>
-      <tb>{employee.dob.age}</tb>
+      <tb scope="row">{employee.name.first} {employee.name.last}</tb>
+      <tb scope="row">{employee.dob.age}</tb>
+      <tb scope="row"><img src={employee.picture.medium} alt="employee"/></tb>
       </tr>
+      </tbody>
+
      ))}
      </table>
+     </div>
     </div>
   );
 }
